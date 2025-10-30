f618cc9ad78fdb7c92ecc821f22496abb2557008
Sanchita76 <cse2022022@rcciit.org.in>
2025-10-30 16:30:51 +0530

    fixed get method

--- backend/server.js
+++ backend/server.js
@@ -61,9 +61,15 @@ app.use(
     })
 );
 
+app.get("/", (req,res) => {
+  console.log("Server is running...");
+  res.status(200).json({message: "API is running..."});
+});
+
 //Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
-    console.log(`Server is running on port ${PORT}`));
+  console.log(`Server is running on port ${PORT}`)
+);
 
  
