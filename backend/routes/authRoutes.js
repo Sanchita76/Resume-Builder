815f60425caa2195b6109b726a563d2cff33d6aa
Sanchita76 <cse2022022@rcciit.org.in>
2025-11-04 17:53:50 +0530

    changed backend url hardcoded in authRoutes.js from apiPaths.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -17,9 +17,11 @@ router.post("/upload-image", upload.single("image"), (req,res)=>{
     if(!req.file){
         return res.status(400).json({message:"No file uploaded"});
     }
-    const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${
-        req.file.filename
-    }`;
+    // const imageUrl=`${req.protocol}://${req.get("host")}/uploads/${
+    //     req.file.filename
+    // }`;
+    const imageUrl = `https://resume-builder-ujy5.onrender.com/uploads/${req.file.filename}`;
+
     res.status(200).json({imageUrl});
 });
 
